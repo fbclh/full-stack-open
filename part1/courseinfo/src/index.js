61fded1f57d8915d9e157eb28454dcce35c272fd
Fabio Coelho <fbclh@protonmail.com>
2022-04-06 10:46:21 -0300

    Organize styles and folders

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import './normalize.css';
+import './styles/index.css';
+import './styles/normalize.css';
 import { App } from './App';
 
 ReactDOM.render(
