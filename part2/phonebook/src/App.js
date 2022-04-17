515188165f66962852683cf594a410d1c074b187
Fabio Coelho <fbclh@protonmail.com>
2022-04-17 20:26:26 -0300

    Fi the search bar input logic

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 export const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '666-123456', id: 1 }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -15,17 +19,32 @@ export const App = () => {
       id: persons.length + 1
     }
 
-    const isDuplicate = persons.some((person) => person.name === newName)
-
-    if (isDuplicate) {
+    if (persons.some((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
       setNewName('')
     } else {
       setPersons(persons.concat(newPerson))
+      // setPersons([...persons, newPerson])
       setNewName('')
       setNewNumber('')
     }
   }
+
+  // const handleSearch = (event) => {
+  //   const searchName = event.target.value
+  //   const filteredPersons = persons.filter((person) =>
+  //     person.name.toLowerCase().includes(searchName.toLowerCase())
+  //   )
+  //   setPersons(filteredPersons)
+  // }
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+  }
+
+  const personsToShow = search
+    ? persons.filter((person) => person.name.toLowerCase().includes(search))
+    : persons
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -37,7 +56,9 @@ export const App = () => {
 
   return (
     <>
-      <h2>Phone book</h2>
+      <h1>Phonebook</h1>
+      <input type="text" placeholder="Search" onChange={handleSearch} />
+      {/* <h2>Add Contact</h2> */}
       <form onSubmit={handleSubmit}>
         <div>
           <input
@@ -63,7 +84,7 @@ export const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
+        {personsToShow.map((person) => (
           <li key={person.id}>
             {person.name}
             {': '}
